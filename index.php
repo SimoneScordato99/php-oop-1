50916dc5e910ef3d3cb5234ae29f882dc75ab8ab
SimoneScordato99 <scordato.simone@yahoo.com>
2023-05-31 16:44:49 +0200

    bonus1

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 
 class Movie{
     public $titolo;
-    public $genere;
+    public $generi;
     public $durata;
 
-    public function __construct($_titolo, $_genere, $_durata){
+    public function __construct($_titolo, Generi $_generi, $_durata){
         $this->titolo = $_titolo;
-        $this->genere = $_genere;
+        $this->generi = $_generi;
         $this->durata = $_durata;
     }
     public function mario($_titoloUpp){
@@ -17,19 +17,36 @@ class Movie{
 
 }
 
-$avengers = new Movie('avengers', 'shi-fi', '2:30');
-$titanic = new Movie('titanic', 'drammatico', '3:00');
+class Generi{
+    public $genere1;
+    public $genere2;
+    public $genere3;
+
+    public function __construct($_genere1 , $_genere2 , $_genere3 ){
+        $this->genere1 = $_genere1;
+        $this->genere2 = $_genere2;
+        $this->genere3 = $_genere3;
+    }
+
+}
+
+$avengers = new Movie('avengers', new Generi('maiale', 'porco', 'suino'), '2:30');
+$titanic = new Movie('titanic', new Generi('cavallo', 'giumenta', 'pony'), '3:00');
 var_dump($avengers , $titanic);
 echo $avengers->titolo;
-echo $avengers->genere;
+echo $avengers->generi->genere1;
+echo $avengers->generi->genere2;
+echo $avengers->generi->genere3;
 echo $avengers->durata;
 
 echo $titanic->titolo;
-echo $titanic->genere;
+echo $titanic->generi->genere1;
+echo $titanic->generi->genere2;
+echo $titanic->generi->genere3;
 echo $titanic->durata;
 
 
-var_dump($titanic);
 echo $titanic -> mario('titanic');
+var_dump($titanic);
 
 ?>
